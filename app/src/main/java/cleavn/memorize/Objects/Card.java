27afdca9893c3d2f520bfc2570ad16a5d7dd8d7f
Cleavn <vdnguyen91@gmail.com>
2018-05-05 18:43:44 +0200

    ToonActivity Dialog

---
+++ app/src/main/java/cleavn/memorize/Objects/Card.java
@@ -0,0 +1,45 @@
+package cleavn.memorize.Objects;
+
+public class Card {
+    private String question, answer;
+    private int _id;
+    private int categoryId;
+
+    public Card(String question, String answer, int categoryId){
+        this.question = question;
+        this.answer = answer;
+        this.categoryId = categoryId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int _id) {
+        this._id = _id;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+}
