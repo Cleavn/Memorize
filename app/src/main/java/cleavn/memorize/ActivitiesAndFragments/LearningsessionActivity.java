8adb64dbde56b3645bb8474d0a2bfca1ac6972da
Cleavn <vdnguyen91@gmail.com>
2018-06-11 14:59:06 +0200

    MemoQ: Surface

---
+++ app/src/main/java/cleavn/memorize/ActivitiesAndFragments/LearningsessionActivity.java
@@ -0,0 +1,18 @@
+package cleavn.memorize.ActivitiesAndFragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import cleavn.memorize.R;
+
+public class LearningsessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.learningsession_layout);
+
+
+    }
+}
